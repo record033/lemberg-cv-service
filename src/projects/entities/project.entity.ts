be322291ea16c9d975a022083522d36404da1a3b
Denys Levi <denys.levi@lemberg.co.ua>
2021-07-05 16:23:10 +0300

    project endpoints services and entity+dto

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,11 @@
-export class Project {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn('increment')
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  description: string;
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { Project } from './entities/project.entity';
 
 @Injectable()
 export class ProjectsService {
+  constructor(@InjectRepository(Project) private readonly repo: Repository<Project>) {}
+
   create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+    const newEntity = new Project();
+
+    newEntity.name = createProjectDto.name;
+    newEntity.description = createProjectDto.description;
+
+    return this.repo.save(newEntity);
   }
 
   findAll() {
-    return `This action returns all projects`;
+    return this.repo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} project`;
+    return this.repo.findOne(id);
   }
 
   update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+    const newEntity = new Project();
+
+    newEntity.name = updateProjectDto.name;
+    newEntity.description = updateProjectDto.description;
+
+    return this.repo.update(id, newEntity);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} project`;
+    return this.repo.delete(id);
   }
 }
