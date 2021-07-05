27c8c4017799946ae6ca042f3f71d5ba2bb7cbac
Denys Levi <denys.levi@lemberg.co.ua>
2021-07-05 16:32:29 +0300

    registred entity in module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Project } from './entities/project.entity';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Project])],
   controllers: [ProjectsController],
-  providers: [ProjectsService]
+  providers: [ProjectsService],
 })
 export class ProjectsModule {}
