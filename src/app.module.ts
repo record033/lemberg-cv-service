297df199d48a6004f7a11c0fa77c371a07905188
Denis Levy <record033@gmail.com>
2021-07-05 12:51:50 +0300

    linting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { config } from '../config';
+
+import { TechnologiesModule } from './technologies/technologies.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from '../config';
-import { TechnologiesModule } from './technologies/technologies.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-    }),
-    TechnologiesModule,
-  ],
+  imports: [TypeOrmModule.forRoot({}), TechnologiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
